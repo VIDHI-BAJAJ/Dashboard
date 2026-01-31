19727e7464e4d931fb5049de7cf5343ace9e58e0
Vidhi Bajaj <vidhibajaj1401@gmail.com>
2026-01-31 16:22:16 +0530

    Frontend update

--- client/src/CRMAnalytics.jsx
+++ client/src/CRMAnalytics.jsx
@@ -831,5 +831,6 @@ function IconMinus() {
     <svg width="14" height="14" viewBox="0 0 24 24" fill="none" className="block">
       <path d="M5 12h14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
     </svg>
+    
   );
 }
