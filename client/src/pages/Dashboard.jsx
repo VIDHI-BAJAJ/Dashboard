c6baf50e31c6e912937a7c3c92565b50e2d34ee9
Vidhi Bajaj <vidhibajaj1401@gmail.com>
2026-02-12 14:27:15 +0530

    frontend update

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -118,6 +118,7 @@ export default function Dashboard() {
       {/* Mobile: Stack vertically, Tablet: 60/40 split, Desktop: 70/30 split */}
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 mt-8 items-stretch">
 
+
   {/* Revenue */}
   <div className="lg:col-span-3">
     <RevenueCard />
